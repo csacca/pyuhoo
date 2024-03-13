489cc9853a686d5b1fdcd78cd9b363f7a79eb9df
Will Rouesnel <wrouesnel@wrouesnel.com>
2024-03-14 10:17:59 +1100

    Mitigate lingering timer failure.

--- tests/test_client_live.py
+++ tests/test_client_live.py
@@ -22,7 +22,16 @@ if os.getenv("UHOO_PASSWORD") is None:
         allow_module_level=True,
     )
 
-async def test_get_latest_data(username, password):
+@pytest.fixture()
+def expected_lingering_timers() -> bool:
+    """Temporary ability to bypass test failures.
+    Parametrize to True to bypass the pytest failure.
+    @pytest.mark.parametrize("expected_lingering_timers", [True])
+    This should be removed when all lingering timers have been cleaned up.
+    """
+    return True
+
+async def test_get_latest_data(username, password, expected_lingering_timers):
     async with ClientSession() as _websession:
         client = Client(username, password, _websession)
         await client.login()
